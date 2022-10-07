3bc77e3cd41061ed11e9270e3fc341de3ce0b966
=BjChacha <bjchacha@outlook.com>
2022-10-07 23:14:24 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+  "overrides": [
+  ],
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react"
+  ],
+  "ignorePatterns": ['/**/node_modules/*', '/node_modules/'],
+  "rules": {
+  },
+}
